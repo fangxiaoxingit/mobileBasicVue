878d7d1e69f5a7c2639b1cbd11cecaa84c5a2434
方小新 <431290494@qq.com>
2020-01-06 11:21:24 +0800

    引入全局less

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,8 +5,7 @@
     <div class="index_2">Hello World! </div>
     <i class="iconfont icon-shuju1"></i> <br>
     <button class="btn">按钮</button> <br>
-    <button class="btn btn-50">按钮50</button>
-    <button class="btn btn-100">按钮100</button>
+    <p class="text-theme-color">主题色</p>
   </div>
 </template>
 
@@ -26,21 +25,27 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
-@import "../assets/less/index.less";
 .iconfont{
-  color: #42b983;
+  color: @theme-color;
   font-size: 30px;
 }
 .index{
   width: 750px;
   font-size: 30px;
-  background: @color-background;
-  color: @color-theme;
   line-height: 3;
 }
 .index_2{
   font-size: 30px;
   line-height: 3;
+}
+.text-theme-color{
+  color: @theme-color;
+}
+.btn{
+  border: none;
+  color: #fff;
+  padding: 10px 20px;
+  background-color: @theme-color;
 }
 h1{
   font-size: 50px;
